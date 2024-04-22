b07f3f4d0ff80212e5a35bc42e3755b056a68625
Peter Street <lazerfx@gmail.com>
2024-04-22 11:14:12 +0100

    Test project set up, initial class library set up, first red test created

---
+++ src/test/bank-ocr-test/TextGenerator.cs
@@ -0,0 +1,10 @@
+﻿
+namespace bank_ocr_test;
+
+public class TextGenerator
+{
+    internal static object CreateNumber(int input, string expectedOutput)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/test/bank-ocr-test/TextGeneratorTests.cs
@@ -0,0 +1,25 @@
+/*
+    The Text Generator is utilised throughout the test framework for the `bank-ocr`.  It's going to take numerical input, and output it into textual strings.
+
+    Each numerical character will have its own output.
+
+    NOTE: Newlines should be never assumed to be either \r\n or \n only, always use `Environment.Newline`.  This sounds obvious, but writing out serves as
+        an aide memoire and a deliberate statement of intent.
+*/
+
+namespace bank_ocr_test;
+
+public class TextGeneratorTests
+{
+    public static IEnumerable<object[]> CreateNumber_DataSource() {
+        yield return new object[] { 1, "  |" + Environment.NewLine + "  |" };
+    }
+
+    [Theory]
+    [MemberData(nameof(CreateNumber_DataSource))]
+    public void CreateNumber_WillOutput_TheRightValue(int input, string expectedOutput)
+    {
+        var output = TextGenerator.CreateNumber(input, expectedOutput);
+        output.Should().Be(expectedOutput);
+    }
+}
