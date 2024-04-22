c1ca9495a85054bb4e22730c99aa433b766200b9
Peter Street <lazerfx@gmail.com>
2024-04-22 11:35:43 +0100

    Initial generation is complete

--- src/test/bank-ocr-test/TextGenerator.cs
+++ src/test/bank-ocr-test/TextGenerator.cs
@@ -5,7 +5,14 @@ public class TextGenerator
 {
     public static Dictionary<int, string> NumberData = new() {
         { 1, "   " + Environment.NewLine + "  |" + Environment.NewLine + "  |" },
-        { 2, " _ " + Environment.NewLine + " _|" + Environment.NewLine + "|_ " }
+        { 2, " _ " + Environment.NewLine + " _|" + Environment.NewLine + "|_ " },
+        { 3, " _ " + Environment.NewLine + " _|" + Environment.NewLine + " _|" },
+        { 4, "   " + Environment.NewLine + "|_|" + Environment.NewLine + "  |" },
+        { 5, " _ " + Environment.NewLine + "|_ " + Environment.NewLine + " _|" },
+        { 6, " _ " + Environment.NewLine + "|_ " + Environment.NewLine + "|_|" },
+        { 7, " _ " + Environment.NewLine + "  |" + Environment.NewLine + "  |" },
+        { 8, " _ " + Environment.NewLine + "|_|" + Environment.NewLine + "|_|" },
+        { 9, " _ " + Environment.NewLine + "|_|" + Environment.NewLine + " _|" },
     };
 
     internal static object CreateNumber(int input, string expectedOutput)
