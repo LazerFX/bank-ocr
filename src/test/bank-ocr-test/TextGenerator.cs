b284d7e4aec16bd6ffde66dc98b3e903269733ba
Peter Street <lazerfx@gmail.com>
2024-04-22 11:29:10 +0100

    Green for 2nd test - can proceed faster now

--- src/test/bank-ocr-test/TextGenerator.cs
+++ src/test/bank-ocr-test/TextGenerator.cs
@@ -3,8 +3,16 @@ namespace bank_ocr_test;
 
 public class TextGenerator
 {
+    public static Dictionary<int, string> NumberData = new() {
+        { 1, "   " + Environment.NewLine + "  |" + Environment.NewLine + "  |" },
+        { 2, " _ " + Environment.NewLine + " _|" + Environment.NewLine + "|_ " }
+    };
+
     internal static object CreateNumber(int input, string expectedOutput)
     {
-        return "   " + Environment.NewLine + "  |" + Environment.NewLine + "  |";
+        if (NumberData.TryGetValue(input, out var retval)) {
+            return retval;
+        }
+        return "";
     }
 }
