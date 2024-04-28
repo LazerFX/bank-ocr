e5bf00d3d3f14caee281db49d7e6ba17f72cfa8e
Peter Street <lazerfx@gmail.com>
2024-04-28 18:36:18 +0100

    Added new function and first test

--- src/bank-ocr/CharacterRecogniser.cs
+++ src/bank-ocr/CharacterRecogniser.cs
@@ -1,10 +1,18 @@
 
 namespace bank_ocr;
 
-public class CharacterRecogniser
+public static class CharacterRecogniser
 {
     public static IEnumerable<string> GetCharacters(string digits)
     {
-        return [0];
+        var inputLines = digits.Split(Environment.NewLine);
+        var output = new List<string>();
+        var splitLine1 = inputLines[0].SplitInto3s();
+
+        return [];
+    }
+
+    public static IEnumerable<string> SplitInto3s(this string input) {
+        throw new NotImplementedException();
     }
 }
--- src/test/bank-ocr-test/CharacterRecogniserTest.cs
+++ src/test/bank-ocr-test/CharacterRecogniserTest.cs
@@ -10,13 +10,23 @@ public class CharacterRecogniserTest
                     "| || |" + Environment.NewLine +
                     "|_||_|" + Environment.NewLine;
         
-        var individiualZero = " _ " + Environment.NewLine +
-                            "| |" + Environment.NewLine +
-                            "|_|" + Environment.NewLine;
+        var individiualZero =   " _ " + Environment.NewLine +
+                                "| |" + Environment.NewLine +
+                                "|_|" + Environment.NewLine;
         
         var expectedOutput = new[] { individiualZero, individiualZero };
 
         var actualOutput = CharacterRecogniser.GetCharacters(input);
+
+        Assert.Equal(expectedOutput, actualOutput);
+    }
+
+    [Fact]
+    public void SplitInto3s_Will_SplitStringIntoArrayOf3CharLongValues() {
+        var input = "111222333";
+        IEnumerable<string> expectedOutput = ["111", "222", "333"];
+
+        var actualOutput = CharacterRecogniser.SplitInto3s(input);
 
         Assert.Equal(expectedOutput, actualOutput);
     }
